df9682d99bf38b02be9524e97e6cc224e0c8adf0
jobzar2025-glitch <jobzar2025@gmail.com>
2025-09-07 16:54:32 +0000

    error change

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
 import { Suspense } from "react";
-import { useRoutes, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/home";
 import ForWorkersPage from "./components/ForWorkersPage";
 import ForClientsPage from "./components/ForClientsPage";
-import routes from "tempo-routes";
 
 function App() {
   return (
     <Suspense fallback={<p>Loading...</p>}>
-      <>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/for-workers" element={<ForWorkersPage />} />
-          <Route path="/for-clients" element={<ForClientsPage />} />
-        </Routes>
-        {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
-      </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/for-workers" element={<ForWorkersPage />} />
+        <Route path="/for-clients" element={<ForClientsPage />} />
+      </Routes>
     </Suspense>
   );
 }
