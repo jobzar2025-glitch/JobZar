79f13b01e4a26f8c01c896fcb423dd3557a2b079
jobzar2025-glitch <jobzar2025@gmail.com>
2025-09-10 14:21:09 +0000

    Navigation again

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/home";
-import ForWorkersPage from "./components/ForWorkersPage";
-import ForClientsPage from "./components/ForClientsPage";
 
 function App() {
   return (
     <Suspense fallback={<p>Loading...</p>}>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/for-workers" element={<ForWorkersPage />} />
-        <Route path="/for-clients" element={<ForClientsPage />} />
+        {/* Redirect any other routes to home */}
+        <Route path="*" element={<Home />} />
       </Routes>
     </Suspense>
   );
